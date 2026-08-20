e500830763beae8054c32b98ee5f288d239b2fb9
sunithamary576 <sunithamary576@gmail.com>
2026-08-21 01:27:41 +0530

    new commit 6

---
+++ PythonLearning/Assign6(2).py
@@ -0,0 +1,13 @@
+s1={"apple","watermelon","banana","papaya"}
+s2={"mango","orange","papaya","kiwi"}
+print(s1 | s2)
+print(s1 & s2)
+print(s1 - s2)
+s2.add("pineapple")
+print(s2)
+s1.remove("banana")
+print(s1)
+s1.discard("muskmelon")
+print(s1)
+
+
---
+++ PythonLearning/Assign6(3).py
@@ -0,0 +1,9 @@
+list=[10,20,30,40,20]
+print(list)
+l2=set((list))
+print(l2)
+l3=tuple((list))
+print(l3)
+l2.add(50)
+print(l2)
+
