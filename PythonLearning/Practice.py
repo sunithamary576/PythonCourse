dc6fc51e67efeefd54c5cd84c54877348f543eaf
sunithamary576 <sunithamary576@gmail.com>
2026-08-31 21:44:51 +0530

    new practice

--- PythonLearning/Practice.py
+++ PythonLearning/Practice.py
@@ -1,6 +1,9 @@
-Bill=210
-coupon=249
-if Bill>=coupon:
+Bill=600
+coupon1=249
+coupon2=500
+if Bill>=coupon1 and Bill<=400:
+    print(f"Total amount to pay: {int(Bill/2)}")
+elif Bill>=coupon2 and Bill<=1000:
     print(f"Total amount to pay: {int(Bill/1.5)}")
 else:
     print(f"Total amount to pay: {Bill}")
