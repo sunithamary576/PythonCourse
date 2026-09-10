eeb5dfcd7647abec4dbe7d4f094b407627e96c4c
sunithamary576 <sunithamary576@gmail.com>
2026-09-10 23:51:09 +0530

    new commit assign 12

---
+++ PythonLearning/Assign12(2).py
@@ -0,0 +1,6 @@
+def greet_user(user):
+    print(f"Welcome {user}")
+
+greet_user("Twinkle")
+greet_user("Tanya")
+greet_user("Britto")
---
+++ PythonLearning/Assign12(3).py
@@ -0,0 +1,8 @@
+def add_numbers(a,b):
+    print(f"Sum of {a} and {b} is {a+b}")
+
+add_numbers(5,5)
+add_numbers(4,8)
+add_numbers(6,8)
+add_numbers(3,7)
+add_numbers(5,7)
